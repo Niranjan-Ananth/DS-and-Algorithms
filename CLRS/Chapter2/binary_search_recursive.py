0eea67740e3bc1c29b4d0eb3e79556c502447d67
Niranjan-Ananth <niran.96@gmail.com>
2021-09-22 23:12:32 +0530

    Added performance testing on binary search

--- CLRS/Chapter2/binary_search_recursive.py
+++ CLRS/Chapter2/binary_search_recursive.py
@@ -1,4 +1,7 @@
-def binary_search(arr, element, low, high):
+import time
+from array_generator import sorted_array_generator, random_number_generator
+
+def binary_search_recursive(arr, element, low, high):
     if low > high:
         return None
     mid = low + int((high-low)/2)
@@ -8,17 +11,39 @@ def binary_search(arr, element, low, high):
         high = mid-1
     else:
         low = mid+1
-    res = binary_search(arr, element, low, high)
+    res = binary_search_recursive(arr, element, low, high)
     return res
+
+
+def test_binary_search_recursive_performance():
+    print("Starting performace test")
+    start_time = time.time()
+    n = 100000
+    max_limit = 500000
+    elements_found = 0
+    iterations = 10000
+    arr = sorted_array_generator(n, max_limit)
+    for i in range(0, iterations):
+        element = random_number_generator(max_limit)        
+        result = binary_search_recursive(arr, element, 0, len(arr)-1)
+        if result:
+            elements_found += 1    
+    end_time = time.time()
+    print("Number of elements found: " + str(elements_found))
+    print("Number of elements not found: " + str(iterations - elements_found))
+    print("Overall time taken: " + str(end_time-start_time))
+    print("Ending performace test")
+
 
 if __name__=='__main__':
     arr = [1, 23, 27, 42, 52, 67, 72, 81, 99, 100]    
     element = 100
     print("Input array: " + str(arr))
     print("Element to be searched: " + str(element))
-    result = binary_search(arr, element, 0, len(arr)-1)
+    result = binary_search_recursive(arr, element, 0, len(arr)-1)
     if result:
         print("Found element at index: " + str(result))
     else:
         print("Could not find element in array")
+    test_binary_search_recursive_performance()
     
